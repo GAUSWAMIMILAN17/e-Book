a50781f7c0b4fb3d97e63dd9bba723717cecb936
Milan Gauswami <gauswamimilan17@gmail.com>
2025-11-25 18:32:32 +0530

    update

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -7,6 +7,7 @@ import {isAdmin} from "../middleware/isAdmin.js"
 
 const router = express.Router();
 
+
 router.route("/addNewBook").post(authenticateToken, isAdmin ,addNewBook);
 router.route("/getAllBooks").get(authenticateToken, getAllBook)
 router.route("/deleteBook/:id").delete(authenticateToken, isAdmin, deleteBook)
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -3,10 +3,18 @@ import express from "express"
 
 import authenticateToken from "../middleware/authenticateToken.js";
 import {isAdmin} from "../middleware/isAdmin.js"
-import { placeOrder } from "../controller/order.controller.js";
+import { getAllOrders, getMyOrders, getOneOrder, getOrder, placeOrder, updateOrderStatus } from "../controller/order.controller.js";
+import { isUser } from "../middleware/isUser.js";
 
 
 const router = express.Router();
 
-router.route("/placeOrder/:id").post(authenticateToken,placeOrder)
+router.route("/placeOrder").post(authenticateToken,isUser,placeOrder)
+router.route("/myOrders").get(authenticateToken,isUser,getMyOrders)
+router.route("/myOrders/:id").get(authenticateToken,isUser,getOrder)
+router.route("/getAllOrders").get(authenticateToken,isAdmin,getAllOrders)
+router.route("/updateOrderStatus/:id").post(authenticateToken,isAdmin,updateOrderStatus)
+router.route("/getOneOrder/:id").get(authenticateToken,isAdmin,getOneOrder)
+
+
 export default router;
